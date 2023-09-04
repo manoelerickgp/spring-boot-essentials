245045b76e5ec78cea3095b776cab2e1f856bb7f
manoelerickgp <manoel.computacao@gmail.com>
2023-09-04 12:52:37 -0300

    Testando start do projeto

---
+++ src/main/java/com/estudo/SpringEstudoDevDojo/controllers/AnimeController.java
@@ -0,0 +1,25 @@
+package com.estudo.SpringEstudoDevDojo.controllers;
+
+import com.estudo.SpringEstudoDevDojo.domain.Anime;
+import com.estudo.SpringEstudoDevDojo.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/animes")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+
+    @GetMapping(path = "/list")
+    public List<Anime> list(){
+        return List.of(new Anime("DBZ"), new Anime("Naruto"), new Anime("One Punch Man"));
+    }
+}
---
+++ src/main/java/com/estudo/SpringEstudoDevDojo/domain/Anime.java
@@ -0,0 +1,18 @@
+package com.estudo.SpringEstudoDevDojo.domain;
+
+public class Anime {
+
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
