e0bcd45bfa9c8c0a1a1c7d93c3d105c920873284
manoelerickgp <manoel.computacao@gmail.com>
2023-09-04 12:39:42 -0300

    first commit

---
+++ src/main/java/com/estudo/SpringEstudoDevDojo/SpringEstudoDevDojoApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.SpringEstudoDevDojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEstudoDevDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEstudoDevDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/SpringEstudoDevDojo/SpringEstudoDevDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.SpringEstudoDevDojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEstudoDevDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
