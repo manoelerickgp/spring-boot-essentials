767716fbefbe7f50b6c108557e52dee760450067
manoelerickgp <manoel.computacao@gmail.com>
2023-09-04 16:04:02 -0300

    Adicionando momento de requisicao com log4j2

--- src/main/java/com/estudo/SpringEstudoDevDojo/controllers/AnimeController.java
+++ src/main/java/com/estudo/SpringEstudoDevDojo/controllers/AnimeController.java
@@ -2,12 +2,14 @@ package com.estudo.SpringEstudoDevDojo.controllers;
 
 import com.estudo.SpringEstudoDevDojo.domain.Anime;
 import com.estudo.SpringEstudoDevDojo.util.DateUtil;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -15,11 +17,12 @@ import java.util.List;
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
-
+    
     private final DateUtil dateUtil;
 
     @GetMapping(path = "/list")
     public List<Anime> list(){
-        return List.of(new Anime("DBZ"), new Anime("Naruto"), new Anime("One Punch Man"));
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("DBZ"), new Anime("Naruto"), new Anime("One Punch Man2"));
     }
 }
